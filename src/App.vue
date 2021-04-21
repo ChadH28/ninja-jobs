1d80bd1b01a144917a1f6e1e85985894b3190348
ChadH28 <chadhoosain@gmail.com>
2021-04-21 11:55:40 +0200

    deleted helloworld component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{name: 'Home'}">Home</router-link> |
+      <router-link :to="{name: 'About'}">About</router-link>
     </div>
     <router-view />
   </div>
@@ -24,9 +24,14 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: royalblue;
+  padding: 10px 5px;
+  border-radius: 5px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus placeat saepe aspernatur fugiat eveniet deleniti molestias necessitatibus aut veritatis, modi vel ipsam, atque a velit error minima alias distinctio culpa. Ad qui officiis pariatur expedita praesentium, iure voluptatem doloremque nostrum! Eaque odio reiciendis saepe voluptates dolores error excepturi dolor dolorum, veniam tempora facere, quibusdam inventore maxime suscipit cum animi. Doloribus eos dignissimos quaerat ut provident accusantium sunt tenetur sed, modi at, consectetur obcaecati? Placeat possimus vitae obcaecati, dolorem ipsa reiciendis quam eos quisquam. Commodi repellat fugiat at distinctio consequuntur assumenda sapiente modi dolor officia animi unde nobis deleniti expedita temporibus ipsum in, sequi laboriosam iste recusandae harum a ut inventore quasi maxime. Nemo maiores ab consequuntur sapiente quis consectetur totam, officia modi iure blanditiis nisi natus accusantium rem. Minus eligendi neque tenetur voluptas quam dolores, at eos rem ullam, laboriosam rerum harum ratione molestiae aliquam tempore assumenda, in magni corporis. Consectetur quod nobis natus error voluptatum voluptatem dolore fuga repudiandae qui, odit ipsum quibusdam necessitatibus recusandae, cumque quasi corporis ullam hic! Cupiditate impedit dolore atque! Ab, libero maiores quam quae, debitis odit sequi amet, veritatis excepturi voluptates quis itaque similique recusandae. Nemo, voluptatum? Sit a et voluptatem harum ratione expedita?</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
