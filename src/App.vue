1ed6734c21c6a241189d305cf637114058dee775
ChadH28 <chadhoosain@gmail.com>
2021-04-21 14:36:45 +0200

    Worked on router history and redirects

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
     <div id="nav">
-      <router-link :to="{name: 'Home'}">Home</router-link> |
-      <router-link :to="{name: 'About'}">About</router-link> |
-      <router-link :to="{name: 'Jobs'}">Jobs</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link> |
+      <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
     </div>
+
+    <div class="buttonContainer">
+      <!-- redirect -->
+      <button @click="redirect">&#8734;</button>
+      <!-- back -->
+      <button @click="back">&#8592;</button>
+      <!-- forward -->
+      <button @click="forward">&#8594;</button>
+    </div>
+
     <router-view />
   </div>
 </template>
+
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    back() {
+      // -1 means go back, like going backwards in a index
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    },
+  },
+}
+</script>
+
+
 
 <style>
 #app {
@@ -18,6 +48,13 @@
   color: #2c3e50;
 }
 
+.buttonContainer button{
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+}
+
 #nav {
   padding: 30px;
 }
@@ -26,7 +63,6 @@
   font-weight: bold;
   color: #2c3e50;
   text-decoration: none;
-
 }
 
 #nav a.router-link-exact-active {
